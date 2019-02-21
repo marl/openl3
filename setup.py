72e51e92969f8eb6245d1898ebf762323a134b58
Jason Cramer <jason.t.cramer@gmail.com>
2019-02-20 22:31:18 -0500

    Update kapre to fix decibel normalization issue

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
         'keras==2.0.9',
         'numpy>=1.13.0',
         'scipy>=0.19.1',
-        'kapre>=0.1.3.1',
+        'kapre>=0.1.4',
         'PySoundFile>=0.9.0.post1',
         'resampy>=0.2.1,<0.3.0',
         'h5py>=2.7.0,<3.0.0',
