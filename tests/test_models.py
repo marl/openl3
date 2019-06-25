9013e5d20e475b3e85603d0f5680f2061734a1a3
Jason Cramer <jason.t.cramer@gmail.com>
2019-06-25 12:17:21 -0400

    Update model tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,10 @@
-from openl3.models import load_audio_embedding_model, load_audio_embedding_model_path
+from openl3.models import load_audio_embedding_model, \
+                          get_audio_embedding_model_path, \
+                          load_image_embedding_model, \
+                          get_image_embedding_model_path
 
 
-def test_load_embedding_model_path():
+def test_load_audio_embedding_model_path():
     embedding_model_path = load_audio_embedding_model_path('linear', 'music')
     assert '/'.join(embedding_model_path.split('/')[-2:]) == 'openl3/openl3_audio_linear_music.h5'
 
@@ -21,7 +24,7 @@ def test_load_embedding_model_path():
     assert '/'.join(embedding_model_path.split('/')[-2:]) == 'openl3/openl3_audio_mel256_env.h5'
 
 
-def test_load_embedding_model():
+def test_load_audio_embedding_model():
     m = load_audio_embedding_model('linear', 'music', 6144)
     assert m.output_shape[1] == 6144
 
@@ -56,4 +59,62 @@ def test_load_embedding_model():
     assert m.output_shape[1] == 6144
 
     m = load_audio_embedding_model('mel256', 'env', 512)
+    assert m.output_shape[1] == 512
+
+
+def test_load_image_embedding_model_path():
+    embedding_model_path = load_image_embedding_model_path('linear', 'music')
+    assert '/'.join(embedding_model_path.split('/')[-2:]) == 'openl3/openl3_image_linear_music.h5'
+
+    embedding_model_path = load_image_embedding_model_path('linear', 'env')
+    assert '/'.join(embedding_model_path.split('/')[-2:]) == 'openl3/openl3_image_linear_env.h5'
+
+    embedding_model_path = load_image_embedding_model_path('mel128', 'music')
+    assert '/'.join(embedding_model_path.split('/')[-2:]) == 'openl3/openl3_image_mel128_music.h5'
+
+    embedding_model_path = load_image_embedding_model_path('mel128', 'env')
+    assert '/'.join(embedding_model_path.split('/')[-2:]) == 'openl3/openl3_image_mel128_env.h5'
+
+    embedding_model_path = load_image_embedding_model_path('mel256', 'music')
+    assert '/'.join(embedding_model_path.split('/')[-2:]) == 'openl3/openl3_image_mel256_music.h5'
+
+    embedding_model_path = load_image_embedding_model_path('mel256', 'env')
+    assert '/'.join(embedding_model_path.split('/')[-2:]) == 'openl3/openl3_image_mel256_env.h5'
+
+
+def test_load_image_embedding_model():
+    m = load_image_embedding_model('linear', 'music', 8192)
+    assert m.output_shape[1] == 8192
+
+    m = load_image_embedding_model('linear', 'music', 512)
+    assert m.output_shape[1] == 512
+
+    m = load_image_embedding_model('linear', 'env', 8192)
+    assert m.output_shape[1] == 8192
+
+    m = load_image_embedding_model('linear', 'env', 512)
+    assert m.output_shape[1] == 512
+
+    m = load_image_embedding_model('mel128', 'music', 8192)
+    assert m.output_shape[1] == 8192
+
+    m = load_image_embedding_model('mel128', 'music', 512)
+    assert m.output_shape[1] == 512
+
+    m = load_image_embedding_model('mel128', 'env', 8192)
+    assert m.output_shape[1] == 8192
+
+    m = load_image_embedding_model('mel128', 'env', 512)
+    assert m.output_shape[1] == 512
+
+    m = load_image_embedding_model('mel256', 'music', 8192)
+    assert m.output_shape[1] == 8192
+
+    m = load_image_embedding_model('mel256', 'music', 512)
+    assert m.output_shape[1] == 512
+
+    m = load_image_embedding_model('mel256', 'env', 8192)
+    assert m.output_shape[1] == 8192
+
+    m = load_image_embedding_model('mel256', 'env', 512)
     assert m.output_shape[1] == 512
