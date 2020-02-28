a6ac4ef583ed0d7a0ca4b59cb1074f5d74aabc37
Jason Cramer <jason.t.cramer@gmail.com>
2020-02-28 13:01:26 -0500

    Pin kapre==0.1.4

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
         'keras>=2.0.9,<2.3.0',
         'numpy>=1.13.0',
         'scipy>=0.19.1',
-        'kapre>=0.1.4',
+        'kapre==0.1.4',
         'PySoundFile>=0.9.0.post1',
         'resampy>=0.2.1,<0.3.0',
         'h5py>=2.7.0,<3.0.0',
